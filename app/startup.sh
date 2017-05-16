3ebe54f8cbf8b3f4ddd757e328049149490992ec
vmatekole <victor@matekole.com>
2017-05-16 02:30:08 +0200

    update packaages and prepare for migrating Nick Dodson templates to React versions

--- app/startup.sh
+++ app/startup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker-compose  -f ../docker-compose.yml up mongo &
-ROOT_URL=http://localhost:5001 MONGO_URL=mongodb://localhost/sovreign7 meteor  run --port 5001 --settings settings.json
+ROOT_URL=http://localhost:5001 MONGO_URL=mongodb://localhost/daap meteor  run --port 5001 --settings settings.json
